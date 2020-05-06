b938235948a14ba3092498963098d799be483c53
Bekbolot <nurmanbetovbekbolot@gmail.com>
2020-05-06 12:41:50 +0600

    articleFront

--- src/main/java/kg/inai/jarkynjasha/controller/ArticleController.java
+++ src/main/java/kg/inai/jarkynjasha/controller/ArticleController.java
@@ -1,22 +1,13 @@
 package kg.inai.jarkynjasha.controller;
 
 import kg.inai.jarkynjasha.entity.Article;
-import kg.inai.jarkynjasha.entity.News;
 import kg.inai.jarkynjasha.model.ArticleModel;
-import kg.inai.jarkynjasha.model.NewsModel;
 import kg.inai.jarkynjasha.service.ArticleService;
-import kg.inai.jarkynjasha.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @CrossOrigin
@@ -26,9 +17,12 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    @GetMapping
-    public List<Article> getAll() {
-        return articleService.findAll();
+
+    @GetMapping(value = "/list")
+    public String getAll(Model model) {
+        List<Article> articleList=articleService.findAll();
+        model.addAttribute("articles", articleList);
+        return "articleList";
     }
 
     @GetMapping(value = "/{id}")
