12a46948e3ac01bc427eaadf96cb6f7076bee2d6
Bekbolot <nurmanbetovbekbolot@gmail.com>
2020-05-07 10:33:52 +0600

    Merge branch 'master' of C:\Users\bekbolot\IdeaProjects\jarkynjasha with conflicts.

--- src/main/java/kg/inai/jarkynjasha/controller/ArticleController.java
+++ src/main/java/kg/inai/jarkynjasha/controller/ArticleController.java
@@ -2,12 +2,15 @@ package kg.inai.jarkynjasha.controller;
 
 import kg.inai.jarkynjasha.entity.Article;
 import kg.inai.jarkynjasha.model.ArticleModel;
+import kg.inai.jarkynjasha.model.NewsModel;
 import kg.inai.jarkynjasha.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @CrossOrigin
@@ -31,9 +34,15 @@ public class ArticleController {
     }
 
 
-    @PostMapping
-    public void create(@RequestBody ArticleModel article) {
-        articleService.create(article);
+    @PostMapping(value = "/create")
+    public String addNews(@Valid @ModelAttribute("articles") ArticleModel articleModel,
+                          BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("add", true);
+            return "articleForm";
+        }
+        articleService.create(articleModel);
+        return "redirect:list";
     }
 
     @DeleteMapping
--- src/main/java/kg/inai/jarkynjasha/service/UserService.java
+++ src/main/java/kg/inai/jarkynjasha/service/UserService.java
@@ -1,12 +1,12 @@
 package kg.inai.jarkynjasha.service;
 
 import kg.inai.jarkynjasha.entity.User;
+import kg.inai.jarkynjasha.model.NewsModel;
+
 import java.util.List;
 
 public interface UserService {
     List<User> findAll();
-
-    User getUserById(Long id);
-
+   User getUserById(Long id);
     void create(User user);
 }
